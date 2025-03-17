60ca68ac566bc799ea4768c1a456232f73d5517b
Deepak sharma <ds284122@gmail.com>
2025-03-17 14:52:50 +0530

    Initial commit

---
+++ ThreadSwiftUI/ThreadSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadSwiftUI
+//
+//  Created by Deepak Sharma on 17/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThreadSwiftUI/ThreadSwiftUI/ThreadSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadSwiftUIApp.swift
+//  ThreadSwiftUI
+//
+//  Created by Deepak Sharma on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
